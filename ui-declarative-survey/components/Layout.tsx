711010109236228540ca8fed75602dd723b5fd39
Taffarel Xavier <taffarel_deus@hotmail.com>
2021-01-27 18:02:11 -0300

    Alteraçõe

--- ui-declarative-survey/components/Layout.tsx
+++ ui-declarative-survey/components/Layout.tsx
@@ -9,10 +9,51 @@ type Props = {
 const Layout = ({ children, title = "This is the default title" }: Props) => (
   <>
     <Head>
-      <title>{title}</title>
+      {/* <title>{title}</title> */}
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="stylesheet" href="/style.css" />
+
+      <title>
+        UI declarative Survey: Ant-designer, Material-UI, Chackra-ui,
+        Styled-system, Semantic-UI-React, React-Bootstrap
+      </title>
+
+      <meta
+        name="twitter:image:src"
+        content="https://ui-declarative-survey.vercel.app/68747470733a2f2f6d6174657269616c2d75692e636f6d2f7374617469632f6c6f676f2e737667.png"
+      />
+      <meta name="twitter:site" content="site" />
+      <meta
+        name="twitter:title"
+        content="UI declarative Survey: Ant-designer, Material-UI, Chackra-ui, Styled-system, Semantic-UI-React, React-Bootstrap"
+      />
+      <meta
+        name="twitter:description"
+        content="Esta é uma pesquisa que avalia de forma sintética a interface de pacotes que usam UI declarative."
+      />
+      <meta
+        name="description"
+        content="Esta é uma pesquisa que avalia de forma sintética a interface de pacotes que usam UI declarative."
+      />
+      <meta
+        property="og:image"
+        content="https://ui-declarative-survey.vercel.app/68747470733a2f2f6d6174657269616c2d75692e636f6d2f7374617469632f6c6f676f2e737667.png"
+      />
+      <meta property="og:site_name" content="ui-declarative-survey" />
+      <meta property="og:type" content="site" />
+      <meta
+        property="og:title"
+        content="UI declarative Survey: Ant-designer, Material-UI, Chackra-ui, Styled-system, Semantic-UI-React, React-Bootstrap"
+      />
+      <meta
+        property="og:url"
+        content="https://ui-declarative-survey.vercel.app/"
+      />
+      <meta
+        property="og:description"
+        content="Esta é uma pesquisa que avalia de forma sintética a interface de pacotes que usam UI declarative."
+      />
     </Head>
     <main className="main">
       {children}
--- ui-declarative-survey/pages/c.tsx
+++ ui-declarative-survey/pages/c.tsx
@@ -5,10 +5,7 @@ const App = () => {
     <>
       <Head>
           <title>UI declarative Survey: Ant-designer, Material-UI, Chackra-ui, Styled-system, Semantic-UI-React, React-Bootstrap</title>
-        {/* <meta
-          name="twitter:image:src"
-          content="https://repository-images.githubusercontent.com/34526884/e3ca9980-dbc4-11ea-9588-d673f3529c19"
-        /> */}
+
         <meta
           name="twitter:image:src"
           content="https://ui-declarative-survey.vercel.app/68747470733a2f2f6d6174657269616c2d75692e636f6d2f7374617469632f6c6f676f2e737667.png"
@@ -30,10 +27,6 @@ const App = () => {
           property="og:image"
           content="https://ui-declarative-survey.vercel.app/68747470733a2f2f6d6174657269616c2d75692e636f6d2f7374617469632f6c6f676f2e737667.png"
         />
-        {/* <meta
-          property="og:image"
-          content="https://repository-images.githubusercontent.com/34526884/e3ca9980-dbc4-11ea-9588-d673f3529c19"
-        /> */}
         <meta property="og:site_name" content="ui-declarative-survey" />
         <meta property="og:type" content="site" />
         <meta
